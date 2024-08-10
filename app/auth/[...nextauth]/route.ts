05809490ba108a549de50f003d6c935ab67d6746
Rusty Divine <rusty.osmyn@gmail.com>
2024-08-10 12:07:56 -0500

    Add logging to routes

--- app/auth/[...nextauth]/route.ts
+++ app/auth/[...nextauth]/route.ts
@@ -1,2 +1,14 @@
-import { handlers } from "auth"
-export const { GET, POST } = handlers
+import { handlers } from "auth";
+import { NextRequest } from "next/server";
+
+export async function POST(req: NextRequest) {
+  const url = new URL(req.url);
+  console.log(`Auth POST ${url.pathname}`);
+  return await handlers.POST(req);
+}
+
+export async function GET(req: NextRequest) {
+  const url = new URL(req.url);
+  console.log(`Auth GET ${url.pathname}`);
+  return await handlers.GET(req);
+}
